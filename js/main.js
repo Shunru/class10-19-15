b583ff27d62c54087e96c6f161d9d2876aa952bc
Shun Xiao <shun.xiao@f1006-10-34127.bmcc.cuny.edu>
2015-10-19 15:14:54 -0400

    more in js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function setup(){
+
+    //create canvas
+    createCanvas(windowWidth, windowHeight);
+    //top box
+
+
+
+
+    drawWindow(); //call the function
+    drawWindow();//call the function
+
+
+}
+
+function draw(){}
+
+function drawWindow(startX,startY){
+    strokeWeight(1);
+    stroke('Green');
+    fill('AntiqueWhite');
+    rect(80,150,150,250);
+
+    strokeWeight(1);
+    stroke('LightCoral');
+    fill('AntiqueWhite');
+    rect(280,150,150,250);
+
+}
