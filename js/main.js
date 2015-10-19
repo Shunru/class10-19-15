dc6ef248eac192b1a86508426b1f343ce1f4028b
Shun Xiao <shun.xiao@f1006-10-34127.bmcc.cuny.edu>
2015-10-19 15:41:57 -0400

    more windows in js

--- js/main.js
+++ js/main.js
@@ -7,23 +7,24 @@ function setup(){
 
 
 
-    drawWindow(); //call the function
-    drawWindow();//call the function
+    drawWindow(50,50,'MediumTurquoise'); //call the function
+    drawWindow(250,50,'Sienna');//call the function
+    drawWindow(450,50,'SteelBlue');//call the function
 
 
 }
 
 function draw(){}
 
-function drawWindow(startX,startY){
+function drawWindow(startX,startY,winColor){
     strokeWeight(1);
     stroke('Green');
-    fill('AntiqueWhite');
-    rect(80,150,150,250);
+    fill(winColor);
+    rect(startX,startY,150,250);
 
-    strokeWeight(1);
-    stroke('LightCoral');
-    fill('AntiqueWhite');
-    rect(280,150,150,250);
+    //strokeWeight(1);
+    //stroke('LightCoral');
+    //fill('AntiqueWhite');
+    //rect(startX,startY,150,250);
 
 }
